3dcfc53aa283b1a5149cd900bb6485840c66750d
Adarsh Raj Anand <adarsh1607077@nitp.ac.in>
2020-07-18 15:28:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
 
 def fizzbuzz(num)
   if num % 15 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif num % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else
-    puts nil
+    nil
   end
 end
